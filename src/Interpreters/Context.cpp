93bb0994f4daf491627f68f1a353e208047da0ce
Raúl Marín <git@rmr.ninja>
2021-06-18 15:44:08 +0200

    WIP

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -2005,16 +2005,6 @@ std::shared_ptr<QueryLog> Context::getQueryLog() const
     return shared->system_logs->query_log;
 }
 
-std::shared_ptr<QueryMaterializationLog> Context::getQueryMaterializationLog() const
-{
-    auto lock = getLock();
-
-    if (!shared->system_logs)
-        return {};
-
-    return shared->system_logs->query_materialization_log;
-}
-
 std::shared_ptr<QueryThreadLog> Context::getQueryThreadLog() const
 {
     auto lock = getLock();
@@ -2025,6 +2015,15 @@ std::shared_ptr<QueryThreadLog> Context::getQueryThreadLog() const
     return shared->system_logs->query_thread_log;
 }
 
+std::shared_ptr<QueryViewsLog> Context::getQueryViewsLog() const
+{
+    auto lock = getLock();
+
+    if (!shared->system_logs)
+        return {};
+
+    return shared->system_logs->query_views_log;
+}
 
 std::shared_ptr<PartLog> Context::getPartLog(const String & part_database) const
 {
