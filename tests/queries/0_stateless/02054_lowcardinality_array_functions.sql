0b98dd557c1ce6aefbeb8a9bfcb3c551a2067313
Raúl Marín <git@rmr.ninja>
2021-10-22 13:49:17 +0200

    Some tests

--- tests/queries/0_stateless/02054_lowcardinality_array_functions.sql
+++ tests/queries/0_stateless/02054_lowcardinality_array_functions.sql
@@ -34,7 +34,8 @@ SELECT 'concatFromLowNullCol', toTypeName(arrayMap(y -> concat(y, ' there'), col
 SELECT 'concatFromLowNullCol', arrayMap(y -> concat(y, ' there'), col) FROM lc_nullable_strings;
 
 
--- TODO: There functions should all return LowCardinality types
+-- TODO: There functions should all return LowCardinality types (or Array(LowCardinality(type))
+-- TODO: Address Expression for function arrayFilter must return UInt8, found Nullable(UInt8) {serverError 43}
 CREATE TABLE lc_numbers (col Array(LowCardinality(UInt64))) engine = Memory AS SELECT [number, number + 2] FROM system.numbers LIMIT 1 OFFSET 2;
 CREATE TABLE lc_nullable_numbers (col Array(LowCardinality(Nullable(UInt64)))) engine = Memory AS SELECT [number, number +2, null] FROM system.numbers LIMIT 1 OFFSET 2;
 
@@ -43,12 +44,35 @@ SELECT 'filterLowConst', arrayFilter(x -> x % 2 == 0, [2, 4]::Array(LowCardinali
 SELECT 'filterLowCol', toTypeName(arrayFilter(x -> x % 2 == 0, col)) FROM lc_numbers;
 SELECT 'filterLowCol', arrayFilter(x -> x % 2 == 0, col) FROM lc_numbers;
 
-SELECT 'sumLowConst', toTypeName(arrayMap(x -> x + 2 == 0, [2, 4]::Array(LowCardinality(UInt64))));
-SELECT 'sumLowConst', arrayMap(x -> x + 2, [2, 4]::Array(LowCardinality(UInt64)));
-SELECT 'sumLowCol', toTypeName(arrayMap(x -> x + 2 == 0, col)) FROM lc_numbers;
-SELECT 'sumLowCol', arrayMap(x -> x + 2, col) FROM lc_numbers;
+SELECT 'filterLowNullConst', toTypeName(arrayFilter(x -> x % 2 == 0, [2, 4, null]::Array(LowCardinality(Nullable(UInt64))))); -- {serverError 43}
+SELECT 'filterLowNullConst', arrayFilter(x -> x % 2 == 0, [2, 4, null]::Array(LowCardinality(Nullable(UInt64)))); -- {serverError 43}
+SELECT 'filterLowNullCol', toTypeName(arrayFilter(x -> x % 2 == 0, col)) FROM lc_nullable_numbers; -- {serverError 43}
+SELECT 'filterLowNullCol', arrayFilter(x -> x % 2 == 0, col) FROM lc_nullable_numbers; -- {serverError 43}
 
-SELECT 'minLowConst', toTypeName(arrayMin([2, 4]::Array(LowCardinality(UInt64))));
-SELECT 'minLowConst', arrayMin([2, 4]::Array(LowCardinality(UInt64)));
-SELECT 'minLowCol', toTypeName(arrayMin(col)) FROM lc_numbers;
-SELECT 'minLowCol', arrayMin(col) FROM lc_numbers;
+SELECT 'sumLowConst', toTypeName(arraySum(x -> x + 2 == 0, [2, 4]::Array(LowCardinality(UInt64))));
+SELECT 'sumLowConst', arraySum(x -> x + 2, [2, 4]::Array(LowCardinality(UInt64)));
+SELECT 'sumLowCol', toTypeName(arraySum(x -> x + 2 == 0, col)) FROM lc_numbers;
+SELECT 'sumLowCol', arraySum(x -> x + 2, col) FROM lc_numbers;
+SELECT 'sumLowConst', toTypeName(arraySum([2, 4]::Array(LowCardinality(UInt64))));
+SELECT 'sumLowConst', arraySum([2, 4]::Array(LowCardinality(UInt64)));
+SELECT 'sumLowCol', toTypeName(arraySum(col)) FROM lc_numbers;
+SELECT 'sumLowCol', arraySum(col) FROM lc_numbers;
+
+SELECT 'sumLowNullConst', toTypeName(arraySum(x -> x + 2 == 0, [2, 4, null]::Array(LowCardinality(Nullable(UInt64))))); -- {serverError 43}
+SELECT 'sumLowNullConst', arraySum(x -> x + 2, [2, 4, null]::Array(LowCardinality(Nullable(UInt64)))); -- {serverError 43}
+SELECT 'sumLowNullCol', toTypeName(arraySum(x -> x + 2 == 0, col)) FROM lc_nullable_numbers; -- {serverError 43}
+SELECT 'sumLowNullCol', arraySum(x -> x + 2, col) FROM lc_nullable_numbers; -- {serverError 43}
+SELECT 'sumLowNullConst', toTypeName(arraySum([2, 4, null]::Array(LowCardinality(Nullable(UInt64))))); -- {serverError 43}
+SELECT 'sumLowNullConst', arraySum([2, 4]::Array(LowCardinality(Nullable(UInt64)))); -- {serverError 43}
+SELECT 'sumLowNullCol', toTypeName(arraySum(col)) FROM lc_nullable_numbers; -- {serverError 43}
+SELECT 'sumLowNullCol', arraySum(col) FROM lc_nullable_numbers; -- {serverError 43}
+
+SELECT 'reduceLowConst', toTypeName(arrayReduce('sum', [2, 4]::Array(LowCardinality(UInt64))));
+SELECT 'reduceLowConst', arrayReduce('sum', [2, 4]::Array(LowCardinality(UInt64)));
+SELECT 'reduceLowCol', toTypeName(arrayReduce('sum', col)) FROM lc_numbers;
+SELECT 'reduceLowCol', arrayReduce('sum', col) FROM lc_numbers;
+
+SELECT 'reduceLowNullConst', toTypeName(arrayReduce('sum', [2, 4, null]::Array(LowCardinality(Nullable(UInt64)))));
+SELECT 'reduceLowNullConst', arrayReduce('sum', [2, 4, null]::Array(LowCardinality(Nullable(UInt64))));
+SELECT 'reduceLowNullCol', toTypeName(arrayReduce('sum', col)) FROM lc_nullable_numbers;
+SELECT 'reduceLowNullCol', arrayReduce('sum', col) FROM lc_nullable_numbers;
